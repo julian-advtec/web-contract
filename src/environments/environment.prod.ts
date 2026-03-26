017a3f154257265e797ed699553a938f1ea62a6f
julian-castanoj <julian.castanoj@udea.edu.co>
2026-03-26 07:38:31 -0500

    netlify 0.01

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.7.56:8090/api'  // Misma estructura, diferente IP/puerto
+  apiUrl: 'https://api-contract.onrender.com/api'  // Misma estructura, diferente IP/puerto
 };
