928ef202409c5e5af415c67603c6501c72b3a525
julian-castanoj <julian.castanoj@udea.edu.co>
2026-05-11 15:12:24 -0500

    Railway 0.3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'api-contract-production.up.railway.app/api'
+  apiUrl: ' https://api-contract-production.up.railway.app/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'api-contract-production.up.railway.app/api'
+  apiUrl: ' https://api-contract-production.up.railway.app/api'
 };
